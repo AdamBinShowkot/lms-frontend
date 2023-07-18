b8d540f07a84faae4bb21ecbfa3b540d35369928
AdamBinShokot <adambinshowkot@gmail.com>
2023-07-18 07:33:13 +0600

    Added Male Form Design DOne && create common config  lists fnc

--- src/components/Store/Donner/Form/index.jsx
+++ src/components/Store/Donner/Form/index.jsx
@@ -22,13 +22,59 @@ import {
 
 const AddNewForm=()=>{
     const dispatch=useDispatch();
-    const bloogGroup=useSelector((state)=>state.common.bloodGroup);
+
+    // get state from redux state
+    const bloodGroup=useSelector((state)=>state.common.bloodGroup);
     const gender=useSelector((state)=>state.common.gender);
+
+    // state assign
+    const [gernderData,setGenderData]=useState([]);
+    const [bloodGroupData,setBloodGroupData]=useState([]);
 
     useEffect(()=>{
         dispatch(getAllBloodGroup());
         dispatch(getAllGender());
-    },[])
+    },[]);
+
+    // For Update Gender
+    useEffect(()=>{
+        let lists=[];
+        if(gender.length){
+            lists=configListForSelectData(gender);
+
+            setGenderData(lists);
+        }
+    },[gender]);
+
+    // For Update Blood Group
+    useEffect(()=>{
+        let lists=[];
+        if(bloodGroup.length){
+            lists=configListForSelectData(bloodGroup);
+
+            setBloodGroupData(lists);
+        }
+    },[bloodGroup])
+
+    // Common Function For Config Lists
+    const configListForSelectData=(lists)=>{
+        let myLists=[...lists];
+
+        let data=[];
+
+        if(myLists.length){
+            myLists.map((mData)=>{
+                let myObj={
+                    key:mData.id,
+                    label:mData.name,
+                    value:mData.id
+                };
+
+                data=[...data,myObj];
+            })
+        }
+        return data;
+    }
     return(
         <>
             <Row>
@@ -357,6 +403,187 @@ const AddNewForm=()=>{
                                         <Input/>
                                     </Form.Item>
                                 </Col>
+                            </Row>
+
+                            <Row>
+                                <Col span={12}>
+                                    <Form.Item
+                                    colon={false}
+                                    tooltip={{
+                                        placement:"bottom",
+                                        title:"Occupation"
+                                    }}
+                                    rules={[
+                                        {
+                                            required:true,
+                                            message:"Occupation Is Required."
+                                        }
+                                    ]}
+                                    name="occupation"
+                                    label="Occupation"
+                                    wrapperCol={{
+                                      span:16  
+                                    }}
+                                    labelCol={{
+                                        span:8
+                                    }}
+                                    >
+                                        <Select
+                                        labelInValue={true}
+                                        optionFilterProp='label'
+                                        showSearch
+                                        />
+                                    </Form.Item>
+                                </Col>
+
+                                <Col span={12}>
+                                    <Form.Item
+                                    colon={false}
+                                    tooltip={{
+                                        placement:"bottom",
+                                        title:"Blood Group"
+                                    }}
+                                    rules={[
+                                        {
+                                            required:true,
+                                            message:"Blood Group Is Required."
+                                        }
+                                    ]}
+                                    name="bloodGroup"
+                                    label="Blood Group"
+                                    wrapperCol={{
+                                      span:16  
+                                    }}
+                                    labelCol={{
+                                        span:8
+                                    }}
+                                    >
+                                        <Select
+                                        options={bloodGroupData}
+                                        labelInValue={true}
+                                        optionFilterProp='label'
+                                        showSearch
+                                        />
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col span={12}>
+                                    <Form.Item
+                                    colon={false}
+                                    tooltip={{
+                                        placement:"bottom",
+                                        title:"Qualification"
+                                    }}
+                                    rules={[
+                                        {
+                                            required:true,
+                                            message:"Qualification Is Required."
+                                        }
+                                    ]}
+                                    name="qualification"
+                                    label="qualification"
+                                    wrapperCol={{
+                                      span:16  
+                                    }}
+                                    labelCol={{
+                                        span:8
+                                    }}
+                                    >
+                                        <Input/>
+                                    </Form.Item>
+                                </Col>
+
+                                <Col span={12}>
+                                    <Form.Item
+                                    colon={false}
+                                    tooltip={{
+                                        placement:"bottom",
+                                        title:"Institute"
+                                    }}
+                                    rules={[
+                                        {
+                                            required:true,
+                                            message:"Institute Is Required."
+                                        }
+                                    ]}
+                                    name="institute"
+                                    label="Institute"
+                                    wrapperCol={{
+                                      span:16  
+                                    }}
+                                    labelCol={{
+                                        span:8
+                                    }}
+                                    >
+                                        <Input/>
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col span={12}>
+                                    <Form.Item
+                                    colon={false}
+                                    tooltip={{
+                                        placement:"bottom",
+                                        title:"Gender"
+                                    }}
+                                    rules={[
+                                        {
+                                            required:true,
+                                            message:"Gender Is Required."
+                                        }
+                                    ]}
+                                    name="gender"
+                                    label="Gender"
+                                    wrapperCol={{
+                                      span:16  
+                                    }}
+                                    labelCol={{
+                                        span:8
+                                    }}
+                                    >
+                                        <Select
+                                        options={gernderData}
+                                        labelInValue={true}
+                                        optionFilterProp='label'
+                                        showSearch
+                                        />
+                                    </Form.Item>
+                                </Col>
+
+                                {/* <Col span={12}>
+                                    <Form.Item
+                                    colon={false}
+                                    tooltip={{
+                                        placement:"bottom",
+                                        title:"Blood Group"
+                                    }}
+                                    rules={[
+                                        {
+                                            required:true,
+                                            message:"Blood Group Is Required."
+                                        }
+                                    ]}
+                                    name="bloodGroup"
+                                    label="Blood Group"
+                                    wrapperCol={{
+                                      span:16  
+                                    }}
+                                    labelCol={{
+                                        span:8
+                                    }}
+                                    >
+                                        <Select
+                                        options={bloodGroupData}
+                                        labelInValue={true}
+                                        optionFilterProp='label'
+                                        showSearch
+                                        />
+                                    </Form.Item>
+                                </Col> */}
                             </Row>
                         </Form>
                     </Card>
